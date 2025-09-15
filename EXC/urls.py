0c6aedf2f93cf7a5d18d5cfd0b321db81ef26de4
AmirArshiaSeifi <amirarshia.seifee@gmail.com>
2025-09-16 01:07:38 +0330

    Update urls.py

--- EXC/urls.py
+++ EXC/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('index/', views.index),
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
